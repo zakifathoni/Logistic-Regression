0884d6dda30aa6a6ec5c3e6c748d28611b2d3efc
Muhammad Zaki Fathoni <112263898+zakifathoni@users.noreply.github.com>
2023-09-25 10:58:37 +0700

    lr.py

--- lr.py
+++ lr.py
@@ -11,14 +11,30 @@ dataset = pd.read_csv(url, names=names)
 dataset['class'] = pd.Categorical(dataset['class'])
 dataset['class'] = dataset['class'].cat.codes
 
-# Define the logistic regression function
+# Definisikan fungsi logistic regression
 def logistic_regression(X, y, num_steps, learning_rate):
-  
+    """
+    Melakukan logistic regression menggunakan gradient descent.
+
+    Args:
+        X (numpy.ndarray): Fitur input dengan bentuk (n_samples, n_features).
+        y (numpy.ndarray): Nilai target dengan bentuk (n_samples,).
+        num_steps (int): Jumlah langkah yang akan dilakukan dalam algoritma gradient descent.
+        learning_rate (float): Tingkat pembelajaran untuk algoritma gradient descent.
+
+    Returns:
+        numpy.ndarray: Bobot yang dioptimalkan dengan bentuk (n_features,).
+
+    """
+
+    # Tambahkan kolom intercept ke X
     intercept = np.ones((X.shape[0], 1))
     X = np.hstack((intercept, X))
-    
+
+    # Inisialisasi bobot dengan nilai nol
     weights = np.zeros(X.shape[1])
-    
+
+    # Gradient descent
     for step in range(num_steps):
         scores = np.dot(X, weights)
         predictions = 1 / (1 + np.exp(-scores))
@@ -26,28 +42,28 @@ def logistic_regression(X, y, num_steps, learning_rate):
         output_error_signal = y - predictions
         gradient = np.dot(X.T, output_error_signal)
         weights += learning_rate * gradient
-        
+
     return weights
 
-# Set the seed for the random number generator
+# Set seed untuk generator angka acak
 np.random.seed(0)
 
-# Define number of folds
+# Tentukan jumlah lipatan (folds)
 k = 5
 
-# Shuffle the dataset
+# Acak dataset
 dataset = dataset.sample(frac=1).reset_index(drop=True)
 
-# Split the dataset into k subsets
+# Bagi dataset menjadi k subset
 subset_size = ceil(len(dataset) / k)
 subsets = [dataset[i*subset_size:(i+1)*subset_size] for i in range(k)]
 
-# Get the number of classes
+# Dapatkan jumlah kelas
 num_classes = len(np.unique(dataset['class'].values))
 
-# Apply K-Fold Cross Validation
+# Terapkan K-Fold Cross Validation
 for i in range(k):
-    # Split the subsets into a training set and a test set
+    # Bagi subset menjadi set pelatihan dan set pengujian
     train = pd.concat(subsets[:i] + subsets[i+1:])
     test = subsets[i]
 
@@ -56,39 +72,39 @@ for i in range(k):
     X_test = test.iloc[:, :-1].values
     y_test = test.iloc[:, -1].values
 
-    # Apply Logistic Regression for each class (One-vs-all)
+    # Terapkan Logistic Regression untuk setiap kelas (One-vs-all)
     weights_all_classes = []
     for c in range(num_classes):
         binary_y_train = np.where(y_train == c, 1, 0)
         weights_c = logistic_regression(X_train, binary_y_train, num_steps=300000, learning_rate=5e-5)
         weights_all_classes.append(weights_c)
 
-    # Print the coefficients and intercept for each fold and each class
-    print('Fold ', i+1)
+    # Cetak koefisien dan intercept untuk setiap lipatan dan setiap kelas
+    print('Lipatan ', i+1)
     for c in range(num_classes):
-        print('Class ', c)
-        print('Coefficients: ', weights_all_classes[c][1:])
+        print('Kelas ', c)
+        print('Koefisien: ', weights_all_classes[c][1:])
         print('Intercept: ', weights_all_classes[c][0])
 
-    # Predict the test set results for each class and choose the class with the highest score
+    # Prediksi hasil set pengujian untuk setiap kelas dan pilih kelas dengan skor tertinggi
     final_scores_all_classes = [np.dot(np.hstack((np.ones((X_test.shape[0], 1)), X_test)), weights_c) for weights_c in weights_all_classes]
     preds = np.argmax(final_scores_all_classes, axis=0)
 
-    print('Misclassified samples: %d' % (y_test != preds).sum())
+    print('Jumlah sampel yang salah diklasifikasikan: %d' % (y_test != preds).sum())
 
-    # Evaluate the model: calculate accuracy, precision, recall and f1-score for each fold
-    
+    # Evaluasi model: hitung akurasi, presisi, recall, dan f1-score untuk setiap lipatan
+
     TP = np.sum((y_test == 1) & (preds == 1))
     TN = np.sum((y_test == 0) & (preds == 0))
     FP = np.sum((y_test == 0) & (preds == 1))
     FN = np.sum((y_test == 1) & (preds == 0))
-    
-    accuracy = (preds == y_test).mean()
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    f1_score = 2 * precision * recall / (precision + recall)
 
-    print('Accuracy: ', accuracy)
-    print('Precision: ', precision)
+    akurasi = (preds == y_test).mean()
+    presisi = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1_score = 2 * presisi * recall / (presisi + recall)
+
+    print('Akurasi: ', akurasi)
+    print('Presisi: ', presisi)
     print('Recall: ', recall)
     print('F1 Score: ', f1_score)
